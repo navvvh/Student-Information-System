2cff8dcdb22af99bf81bd44a85048342dba37458
navvvh <vhanhajjmacaraig21@gmail.com>
2025-11-23 05:24:41 +0800

    Setting up React and Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <h1 className=" text-red-500 text-3xl font-bold underline">
+    <h1 className=" text-blue-500 text-3xl font-bold underline">
       Hello world!
+      hiworld
     </h1>
   )
 }
